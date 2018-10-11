09d3393d3fcc03a9be22e14d7c7109eb3755b57f
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-11 19:17:52 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,11 +3,9 @@ def levitation_quiz
 	loop do 
 	  puts "What is the spell that enacts levit action?"
 	  result = gets.chomp
-	  break if result == "STOP"
+	  break if result == "Wingardium Leviosa"
 	end
-	puts ''
-	  
-	
+	puts 'You passed the quiz!'
 end
 
 
